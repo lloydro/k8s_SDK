0386859c2f94ece8613912874c1545a9007994d2
lloydro <119394547@qq.com>
2020-07-17 17:21:48 +0800

    tz

--- KubeApi/setup.py
+++ KubeApi/setup.py
@@ -12,7 +12,7 @@ from setuptools import find_packages
 
 setup(
     name="KubeApi",
-    version="1.0.14",
+    version="1.0.15",
     author="wangyc",
     author_email="wangyongcheng@ruijie.com.cn",
     description="for auto test",
